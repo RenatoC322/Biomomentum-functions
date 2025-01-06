24c754c7fb92f76160b66f4de20bdae88e5ebe53
Renato Castillo <castillo@biomomentum.com>
2025-01-06 10:03:06 -0500

    Add files for updated library

--- biomomentum/setup.py
+++ biomomentum/setup.py
@@ -1,15 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mach_1_functions',
-    packages=find_packages(include=['mach_1_functions']),
+    name='biomomentum',
+    packages=find_packages(include=['biomomentum']),
     install_requires=[
         "numpy",
         "pandas",
         "scipy",
         "scikit-learn"
     ],
-    version='0.1.3',
+    version='0.1.2',
     description='Mach-1 Analysis Functions',
     author='Renato Castillo',
     author_email="castillo.renato@biomomentum.com"
